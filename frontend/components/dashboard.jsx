8e6a5f2e82d9f282035daa09ccae6165dc8f642c
Yugraj <yugrajjoshi185@gmail.com>
2026-03-07 21:43:46 +0530

    dashboard component

---
+++ frontend/components/dashboard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function Dashboard() {
+  return (<>
+    <link rel="stylesheet" href="/components/dashboard.css" />
+    <div className="dashboard">
+      <h1>DASHBOARD</h1>
+      <div className="progress-status">
+       </div>
+    </div>
+    </>
+  );
+}
+
+export default Dashboard;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,11 @@
+import React from 'react'
+import Dashboard from '/components/dashboard.jsx'
+
 function App() {
   return (
-    <main className="min-h-screen bg-black flex items-center justify-center p-6">
-      <div className="w-full max-w-xl rounded-xl bg-white p-8 shadow-sm border border-slate-200 text-center">
-        <h1 className="text-3xl font-bold text-slate-900">Project Management System</h1>
-        
-        <button className="mt-6 inline-flex items-center rounded-md bg-slate-900 px-4 py-2 text-sm font-medium text-white hover:bg-slate-800">
-          Get Started
-        </button>
-      </div>
-      <div className="absolute bottom-4 left-0 right-0 text-xs shadow-sm text-slate-500 border-t border-slate-200 pt-2">
-       set up the project
-      </div>
-    </main>
+   <>
+   <Dashboard/>
+   </>
   )
 }
 
