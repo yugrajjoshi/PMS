7e8f6eea1293682053858c5acae1582f5b353af5
Yugraj <yugrajjoshi185@gmail.com>
2026-03-07 22:13:46 +0530

    python app connection

--- backend/projects/apps.py
+++ backend/projects/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class ProjectsConfig(AppConfig):
     name = 'projects'
+    user =username = 'projects'
+interface = 'projects.apps.ProjectsConfig'
+list[user,interface] = ['projects.apps.ProjectsConfig']
